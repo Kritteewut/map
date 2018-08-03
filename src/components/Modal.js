d833a706d50e6b4a8b3b6ced19f7f20d3d5bc882
m3thom <39703347+m3thom@users.noreply.github.com>
2018-08-03 10:56:55 +0700

    Update
    LUL

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -30,9 +30,9 @@ const styles = theme => ({
     },
     absolute: {
         position: 'absolute',
-        bottom: theme.spacing.unit * 20,
-        right: theme.spacing.unit,
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        bottom: theme.spacing.unit * 3,
+        right: theme.spacing.unit * 7,
+        background: 'linear-gradient(45deg, #FE6B8B 10%, #FF8E53 90%)',
         color: 'white',
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     },
